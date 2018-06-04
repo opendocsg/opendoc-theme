592eec84b7225f921e299b2768d26a65024aae9b
iewil <liwei@data.gov.sg>
2018-06-04 16:51:43 +0800

    Add styling for nested lists

---
+++ assets/layout.js
@@ -0,0 +1,14 @@
+(function(){
+	try {
+		var list = document.getElementsByTagName("ol");
+		for (var i = 0; i < list.length; i++) {
+			if (list[i].getAttribute("start")){
+				var start_value = list[i].getAttribute("start") - 1
+				list[i].style.counterReset = "section " + start_value.toString()
+			}
+		}
+	} catch (err) {
+		console.log(err)
+	}
+
+})();
