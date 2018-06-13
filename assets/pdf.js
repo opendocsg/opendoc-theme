91d9b4a1008d55e4cf841cdcfa28519b68cbb8bf
Yi Xin Ang <angyixin@gmail.com>
2018-06-13 14:58:40 +0800

    styling pdf button

--- assets/pdf.js
+++ assets/pdf.js
@@ -1,16 +1,29 @@
 // Get content from DOM
 var content = document.getElementById('main-content');
-console.log(content.innerHTML);
 
+var margins = {
+    top: 20,
+    left: 15,
+    width: 160
+  };
 
 // Function to convertTextToPDF
 function convertTextToPDF() {
   var doc = new jsPDF('p','mm','a4');
-  doc.fromHTML(content.innerHTML, 10, 10)
-  doc.save('a4.pdf')
+  doc.fromHTML(
+    content,
+    margins.top,
+    margins.left,
+    {
+  		'width': margins.width // max width of content on PDF
+  	},
+  );
+  doc.save('a4.pdf');
   console.log('yay')
 }
 
 // Add event listener to Save PDF button
 var clickPDF = document.getElementById("convert-to-pdf");
 clickPDF.addEventListener("click",convertTextToPDF, false);
+
+//Give the pdf file name that corresponds to the document
