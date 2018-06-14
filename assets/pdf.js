14e236cd82c4e9023287743e6b9a1607f0f1b22e
Yi Xin Ang <angyixin@gmail.com>
2018-06-14 15:26:56 +0800

    remove jspdf use printjs

--- assets/pdf.js
+++ assets/pdf.js
@@ -1,26 +1,16 @@
-var margins = {
-    top: 20,
-    left: 15,
-    width: 160
-  };
-
 // Function to convertTextToPDF
 function convertTextToPDF() {
-  var doc = new jsPDF('p','mm','a4');
-  var content = document.getElementById('main-content');
-  doc.fromHTML(
-    content,
-    margins.top,
-    margins.left,
-    {
-  		'width': margins.width // max width of content on PDF
-  	},
-  );
-  doc.save('a4.pdf');
+  printJS({
+           printable: 'main-content',
+           type: 'html',
+           css: 'https://opengovsg.github.io/opendoc-theme/assets/main.css',
+           scanStyles: false,
+           style: "body{ font-family: 'Source Sans Pro'}"
+       });
 }
 
 // Add event listener to Save PDF button
-var clickPDF = document.getElementById("convert-to-pdf");
-clickPDF.addEventListener("click",convertTextToPDF, false);
+var pdfButton = document.getElementById("convert-to-pdf");
+pdfButton.addEventListener("click",convertTextToPDF, false);
 
 //Give the pdf file name that corresponds to the document
