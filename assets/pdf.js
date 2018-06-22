ed6a223cde2d18953cca78e69f63151642e15c9c
Yi Xin Ang <angyixin@gmail.com>
2018-06-22 15:22:51 +0800

    print pdf with making all md to html.

--- assets/pdf.js
+++ assets/pdf.js
@@ -1,3 +1,20 @@
+---
+# Jekyll front matter needed to trigger coffee compilation
+---
+
+function printHtml(){
+  var content = {{ site.html_pages | markdownify | jsonify}} ;
+  var html =
+  '<div id="main-content">'
+  + content
+  + '</div>' ;
+  return html;
+
+}
+
+printHtml();
+
+
 // Function to convertTextToPDF
 function convertTextToPDF() {
   printJS({
@@ -14,3 +31,9 @@ var pdfButton = document.getElementById("convert-to-pdf");
 pdfButton.addEventListener("click",convertTextToPDF, false);
 
 //Give the pdf file name that corresponds to the document
+
+//Grab HTML for all the pages
+
+//Stitch all the pages together
+
+//Generate the PDF of the stitched HTML
