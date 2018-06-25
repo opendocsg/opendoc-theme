86527dca00635c2be134ca81c30c680bbe639d60
shen_xujing <shen_xujing@ida.gov.sg>
2018-06-25 15:04:00 +0800

    solve export.md cyclic issue (i.e. its own content not shown in compiled export.html) exclude export.html from navigation list set export.md with separate layout which triggers print modal once document is loaded printpdf button invokes opening export.html in new tab

---
+++ assets/redirect-new-tab.js
@@ -0,0 +1,12 @@
+---
+# Jekyll front matter needed to trigger coffee compilation
+---
+
+// open export.html in new tab where print modal is triggered once loaded
+function redirect() {
+  window.open(window.location.href + "export.html", '_blank');
+}
+
+// Add event listener to Save PDF button
+var pdfButton = document.getElementById("convert-to-pdf");
+pdfButton.addEventListener("click",redirect, false);
--- export.md
+++ export.md
@@ -1,7 +1,11 @@
 ---
+exclude: true
+layout: print
 ---
-
-
+<!-- dynamic combine all other md contents except itself -->
+<!-- inspired from https://codana.me/2014/06/16/excluding-pages-from-top-navigation-in-jekyll/ -->
 {% for site_page in site.html_pages %}
-  {{site_page.content}}
+  {% unless site_page.exclude %}
+    {{site_page.content}}
+  {% endunless %}
 {% endfor %}
