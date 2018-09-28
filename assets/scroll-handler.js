43e93b0807c7a7d6407b959de1441b513ef56c43
Ian Chen <ian-@gmx.com>
2018-09-28 14:26:27 +0800

    Restyled nav menu for mobile view

--- assets/scroll-handler.js
+++ assets/scroll-handler.js
@@ -1,9 +1,10 @@
-// grab an element
-var header = document.querySelector("header")
-var mainContent = document.getElementById("main-content")
-var headroom  = new Headroom(header, {
-    scroller: mainContent,
-    tolerance : 5
-})
-// initialise
-headroom.init()
+// For hiding site header when scrolling on mobile
+(() => {
+    let header = document.querySelector('header')
+    let mainContent = document.getElementById('main-content')
+    let headroom = new Headroom(header, {
+        scroller: mainContent,
+        tolerance: 5
+    })
+    headroom.init()
+})()
