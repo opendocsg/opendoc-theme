e5154659ca2c73fbbdd0b84a2adb97b86a724f82
dlqs <donaldleeqs@gmail.com>
2019-05-30 16:53:44 +0800

    add build command for building lunr index

--- assets/startup/build.sh
+++ assets/startup/build.sh
@@ -5,3 +5,5 @@ echo 'Installing node dependencies'
 npm i html-pdf glob jsdom js-yaml
 node _site/assets/startup/pdf-gen.js
 echo 'Generating PDFs Complete'
+node _site/assets/js/prebuild-lunr-index.js
+echo 'Generating Lunr Index Complete'
