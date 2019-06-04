8c4b3ef04143b1ed947ecf437adce2663a25614a
dlqs <donaldleeqs@gmail.com>
2019-06-04 18:25:15 +0800

    do not generate lunr index when offline is false

--- assets/startup/build.sh
+++ assets/startup/build.sh
@@ -1,9 +1,14 @@
+---
+---
 #!/bin/bash
-
 echo 'Generating PDFs'
 echo 'Installing node dependencies'
 npm i html-pdf glob jsdom js-yaml
 node _site/assets/startup/pdf-gen.js
 echo 'Generating PDFs Complete'
-node _site/assets/startup/prebuild-lunr-index.js
-echo 'Generating Lunr Index Complete'
+if [ "{{ site.offline_search_only }}" == "true" ]; then
+    node _site/assets/startup/prebuild-lunr-index.js
+    echo 'Generating Lunr Index complete'
+else
+    echo 'Skipping Lunr Index'
+fi
