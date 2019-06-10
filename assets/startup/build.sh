1ec8bb3e6868d83a6701e6ed3809195825f1cf85
dlqs <donaldleeqs@gmail.com>
2019-06-10 16:09:58 +0800

    remove prebuilding lunr index

--- assets/startup/build.sh
+++ assets/startup/build.sh
@@ -6,9 +6,3 @@ echo 'Installing node dependencies'
 npm i html-pdf glob jsdom js-yaml
 node _site/assets/startup/pdf-gen.js
 echo 'Generating PDFs Complete'
-if [ "{{ site.offline_search_only }}" == "true" ]; then
-    node _site/assets/startup/prebuild-lunr-index.js
-    echo 'Generating Lunr Index complete'
-else
-    echo 'Skipping Lunr Index'
-fi
