e37bcaf0fe84efb17af17aa9f8b1b04502a38ee6
dlqs <donaldleeqs@gmail.com>
2019-06-18 11:45:31 +0800

    allow caching of cloudfront resources

--- sw.js
+++ sw.js
@@ -1,111 +1,80 @@
 ---
 ---
-// Code from https://github.com/jeremiak/jekyll-offline
+/**
+ * ServiceWorker to cache pages to make them accesible offline
+ * 
+ * Note: The service worker (i.e. this file) must be registered *before* the 
+ * resources that it intends to cache are loaded (via fetch).
+ * 
+ * Note: The URLs to be cached must be whitelisted in Content-Security-Policy
+ * header under connect-src since they are proxied through here, i.e. from script.
+ */
 var urlsToCache = [];
 
-{% for post in site.posts %}
-urlsToCache.push("{{ post.permalink | relative_url }}");
-{% endfor %}
 {% for page in site.pages %}
 {% if page.permalink %}
 urlsToCache.push("{{ page.permalink }}");
-{% endif %}
-{% if page.url %}
+{% elsif page.url %}
 urlsToCache.push("{{ page.url }}");
 {% endif %}
 {% endfor %}
 
-var CACHE_NAME = '{{ site.title | slugify }}-cache-v1';
+var CACHE_NAME = '{{ site.title | slugify }}-cache-v1'
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
-    return cache.addAll(urlsToCache);
-  }).catch(function(err) {
-    console.log('cache add err', err);
-  }));
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
+  event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        console.log('Trying to find response for :' + event.request.url)
-        return response || fetch(event.request).then(function(response) {
-          console.log('Did not find response, proxying request')
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
+    return cache.addAll(urlsToCache);
+  }).catch(function(error) {
+    return error
+  }))
+})
 
-// strategies from the offline cookbook by jake archibald
-// https://jakearchibald.com/2014/offline-cookbook/#serving-suggestions-responding-to-requests
+// Cache-then-update strategy
+// From https://serviceworke.rs/strategy-cache-and-update_service-worker_doc.html
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(cached) {
+        var networked = fetch(event.request)
+          .then(fetchedFromNetwork, unableToResolve)
+          .catch(unableToResolve);
+        return cached || networked;
 
-//{% assign strategy = site.offline.strategy | default: 'cache-then-network' %}
-//{% if strategy == 'cache-only' %}
-//  self.addEventListener('fetch', function(event) {
-//    // If a match isn't found in the cache, the response
-//    // will look like a connection error
-//    event.respondWith(caches.match(event.request));
-//  });
-//{% elsif strategy == 'network-only' %}
-//  self.addEventListener('fetch', function(event) {
-//    event.respondWith(fetch(event.request));
-//    // or simply don't call event.respondWith, which
-//    // will result in default browser behaviour
-//  });
-//{% elsif strategy == 'cache-first-network-fallback' %}
-//  self.addEventListener('fetch', function(event) {
-//    event.respondWith(
-//      caches.match(event.request).then(function(response) {
-//        return response || fetch(event.request);
-//      })
-//    );
-//  });
-//{% elsif strategy == 'network-first-cache-fallback' %}
-//  self.addEventListener('fetch', function(event) {
-//    event.respondWith(
-//      fetch(event.request).catch(function() {
-//        return caches.match(event.request);
-//      })
-//    );
-//  });
-//{% elsif strategy == 'cache-network-race' %}
-//  // Promise.race is no good to us because it rejects if
-//  // a promise rejects before fulfilling. Let's make a proper
-//  // race function:
-//  function promiseAny(promises) {
-//    return new Promise((resolve, reject) => {
-//      // make sure promises are all promises
-//      promises = promises.map(p => Promise.resolve(p));
-//      // resolve this promise as soon as one resolves
-//      promises.forEach(p => p.then(resolve));
-//      // reject if all promises reject
-//      promises.reduce((a, b) => a.catch(() => b))
-//        .catch(() => reject(Error("All failed")));
-//    });
-//  };
-//
-//  self.addEventListener('fetch', function(event) {
-//    event.respondWith(
-//      promiseAny([
-//        caches.match(event.request),
-//        fetch(event.request)
-//      ])
-//    );
-//  });
-//{% elsif strategy == 'cache-then-network' %}
-//  self.addEventListener('fetch', function(event) {
-//    event.respondWith(
-//      caches.open(CACHE_NAME).then(function(cache) {
-//        return fetch(event.request).then(function(response) {
-//          cache.put(event.request, response.clone());
-//          return response;
-//        });
-//      })
-//    );
-//  });
-//{% endif %}
+        function fetchedFromNetwork(response) {
+          var cacheCopy = response.clone();
+          caches.open(CACHE_NAME)
+            .then(function add(cache) {
+              cache.put(event.request, cacheCopy)
+            })
+          return response
+        }
+
+        function unableToResolve () {
+          return new Response('Service Unavailable', {
+            status: 503,
+          })
+        }
+      })
+  )
+})
+
+// Delete stale caches when CACHE_NAME changes
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (keys) {
+        return Promise.all(
+          keys.filter(function (key) {
+              return !key.startsWith(CACHE_NAME)
+            })
+            .map(function (key) {
+              return caches.delete(key)
+            })
+        )
+      })
+  )
+})
